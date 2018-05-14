7ae09a6fd87dcd448cdb0ca241ca648c48813ee5
xiaoqu <1483523635@qq.com>
2018-05-14 14:24:53 +0800

    init commit

---
+++ src/OnlineTeach.Web/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineTeach.Web.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage ="{0}不能为空")]
+        [EmailAddress(ErrorMessage ="{0}地址无效")]
+        [Display(Name = "邮箱")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0}不能为空")]
+        [StringLength(100, ErrorMessage = " {0}长度必须在 {2} 和 {1} 之间", MinimumLength = 6)]
+        [DataType(DataType.Password,ErrorMessage ="密码必须包含一个大写字母和一个小写字母")]
+        [Display(Name = "密码")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "确认密码")]
+        [Compare("Password", ErrorMessage = "两次输入的密码不一致")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ src/OnlineTeach.Web/Startup.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineTeach.Web.Data;
+using OnlineTeach.Web.Models;
+using OnlineTeach.Web.Services;
+
+namespace OnlineTeach.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContextPool<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase("Identity" + Guid.NewGuid()));
+                //options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(options=> {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredUniqueChars = 6;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+
+                //sign out 
+                options.Lockout.AllowedForNewUsers = true;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+
+                //sign in 
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+            services.ConfigureApplicationCookie(config=> {
+                //config.Cookie.Domain = "*.naisi.com";
+                config.Cookie.HttpOnly = true;
+                config.Cookie.Name = "naisiCookie";
+
+                config.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                config.LoginPath = "/Account/Login";
+                config.LogoutPath = "/Account/Logout";
+                config.SlidingExpiration = true;
+
+            });
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
