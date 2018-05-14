d48638d660e86c39b2c50668afdba6d2b8359dfb
xiaoqu <1483523635@qq.com>
2018-05-14 14:33:35 +0800

    finish change password

--- src/OnlineTeach.Web/Models/ManageViewModels/ChangePasswordViewModel.cs
+++ src/OnlineTeach.Web/Models/ManageViewModels/ChangePasswordViewModel.cs
@@ -8,20 +8,20 @@ namespace OnlineTeach.Web.Models.ManageViewModels
 {
     public class ChangePasswordViewModel
     {
-        [Required]
+        [Required(ErrorMessage ="{0}不能为空")]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(Name = "当前密码")]
         public string OldPassword { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessage = "{0}不能为空")]
+        [StringLength(100, ErrorMessage = "{0}长度必须在{2}和{1}之间", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "新密码")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Display(Name = "确认新密码")]
+        [Compare("NewPassword", ErrorMessage = "两次输入的密码不一致")]
         public string ConfirmPassword { get; set; }
 
         public string StatusMessage { get; set; }
