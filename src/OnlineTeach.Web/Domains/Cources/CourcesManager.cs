d3e3d5d74cc41847813c49411fb30be5d568a99d
xiaoqu <1483523635@qq.com>
2018-05-16 11:49:51 +0800

    update cource domain methods

--- src/OnlineTeach.Web/Domains/Cources/CourcesManager.cs
+++ src/OnlineTeach.Web/Domains/Cources/CourcesManager.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace OnlineTeach.Web.Domains.Cources
 {
-    public class CourcesManager:AggrationRoot.AggrationRoot<long>
+    public class CourcesManager : AggrationRoot.AggrationRoot<long>
     {
         private IRepository<CourceItem, long> _courceManagerRepositoty;
 
@@ -15,6 +15,37 @@ namespace OnlineTeach.Web.Domains.Cources
         {
             _courceManagerRepositoty = repository;
         }
-
+        public void CreateCource(string userName, string name, string imageUrl, DateTime startTime, DateTime endTime, GradeType gradeType, string content, double price)
+        {
+            _courceManagerRepositoty.Add(new CourceItem() { Name = name, ImageUrl = imageUrl, StartTime = startTime, EndTime = endTime, Grade = gradeType, Content = content, Price = price, BookCount = 0, State = CourceState.UnAdult, TeacherName = userName });
+        }
+        public IEnumerable<CourceItem> GetUnAdultCources()
+        {
+            return _courceManagerRepositoty.GetList(c => c.State == CourceState.UnAdult);
+        }
+        public IEnumerable<CourceItem> GetAllCources()
+        {
+            return _courceManagerRepositoty.GetList(c => c.State == CourceState.Pass);
+        }
+        public IEnumerable<CourceItem> GetAllCourcesByTeacherName(string teacherName)
+        {
+            return _courceManagerRepositoty.GetList(c => c.TeacherName == teacherName);
+        }
+        public void Pass(long Key)
+        {
+            var cource = GetByKey(key);
+            cource.State = CourceState.Pass;
+            _courceManagerRepositoty.SaveChanges();
+        }
+        public void Deny(long key)
+        {
+            var cource = GetByKey(key);
+            cource.State = CourceState.Deny;
+            _courceManagerRepositoty.SaveChanges();
+        }
+        private CourceItem GetByKey(long key)
+        {
+            return _courceManagerRepositoty.GetByKey(key);
+        }
     }
 }
