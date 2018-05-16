dcb6a65be25e7ab85155ce7af6dfb93f5596b824
xiaoqu <1483523635@qq.com>
2018-05-16 11:50:24 +0800

    finish create and index view

--- src/OnlineTeach.Web/Controllers/CourceController.cs
+++ src/OnlineTeach.Web/Controllers/CourceController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OnlineTeach.Web.Domains.Cources;
+using OnlineTeach.Web.Models.CourceViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,14 +18,33 @@ namespace OnlineTeach.Web.Controllers
         {
             _courceManager = courceManager;
         }
+
+        [TempData]
+        public string StatusMessage { get; set; }
         public IActionResult Index()
         {
-            return View();
+            var list = _courceManager.GetAllCourcesByTeacherName(User.Identity.Name);
+            var model = AutoMapper.Mapper.Map<IEnumerable<CourceItemCreateViewModel>>(list);
+            return View(model);
         }
         [HttpGet]
         public IActionResult Create()
         {
-            return View();
+            var model = new CourceItemCreateViewModel() { StateMessage = StatusMessage };
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CourceItemCreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            _courceManager.CreateCource(User.Identity.Name, model.Name,
+                model.ImageUrl, model.StartTime, model.EndTime, model.Grade, model.Content, model.Price);
+            StatusMessage = "创建成功！";
+            return RedirectToAction(nameof(Create));
         }
     }
 }
